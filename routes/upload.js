584858de7fdbd7323b2abb481df4e46e31b47bfa
only-someone <490072639@qq.com>
2019-04-03 15:18:31 +0800

    上传csv 2

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,5 @@
 var  Papa=require('papaparse')
 var  mongodb = require('mongodb');
-var lineReader = require('line-reader');
 var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
 //连接的数据库为stock
 var  db = new mongodb.Db('ship', server, {safe:true});
@@ -19,18 +18,28 @@ var file_upload=function () {
                         if(err){
                             console.log(err);
                         }else{
-                            for()
-                            //逐行读取csv文件
-                            jason={
-                                "shipinfo":, "发电机功率", '主机平均转速','增压器转速','气缸排气量',
-                                '气缸冷却水温度','燃料油量','柴油量','活塞冷却水温度','机油温度',
-                                '减速箱油压','增压器压强','海水温度','机舱温度', '更新时间'
+                            for(var i=0;i<devices.length();i++){
+                                device={
+                                    "shipinfo":devices[i][0],
+                                    "发电机功率":devices[i][1],
+                                    '主机平均转速':devices[i][2],
+                                    '增压器转速':devices[i][3],
+                                    '气缸排气量':devices[i][4],
+                                    '气缸冷却水温度':devices[i][5],
+                                    '燃料油量':devices[i][6],
+                                    '柴油量':devices[i][7],
+                                    '活塞冷却水温度':devices[i][8],
+                                    '机油温度':devices[i][9],
+                                    '减速箱油压':devices[i][10],
+                                    '增压器压强':devices[i][11],
+                                    '海水温度':devices[i][12],
+                                    '机舱温度':devices[i][13],
+                                    '更新时间':devices[i][14],
+                                }
+                                collection.insert(json,{safe:true},function(err,result){
+                                    console.log(result);
+                                });
                             }
-                        }
-                                //存入数据库
-                            collection.insert(json,{safe:true},function(err,result){
-                                console.log(result);
-                            });
                         }
                     })
                 }
